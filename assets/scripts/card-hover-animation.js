cb2f0ce5198373b0ff9b3112df1efa9563013b1b
GustBitencourt <gubiten@gmail.com>
2022-05-21 21:12:50 -0300

    Corrigindo especificidade do css de animacao

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,16 +1,26 @@
 function handleMouseEnter() {
     //adiciona classe de animação
     this.classList.add('card--hovered');
+
+    /* Trocar background do card */
+    document.body.id = `${this.id}--hovered`;
+}
+
+/* Remove classe responsavel pela animacao quando o mouse sai da imagem */
+function handleMouseLeave() {
+    this.classList.remove('card--hovered');
+    document.body.id = ``
 }
 
 function addEventListenersToCards() {
-    const cardElement = document.getElementsByClassName('card');
+    const cardElements = document.getElementsByClassName('card');
 
-    for(let i = 0; i < cardElement.length; i++) {
+    for(let i = 0; i < cardElements.length; i++) {
         /* cards individuais */
-        const card = cardElement[i];
+        const card = cardElements[i];
         /* chama função com o mouse em cima da imagem */
         card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
     }
 }
 
